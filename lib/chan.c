311042123a2ad5bbe6f49d94c74a43481843d082
StephenPatrick <sythe2o0@gmail.com>
2017-02-10 11:34:45 -0600

    Wrote try_send and try_send _locked.
    Reworked locking logic on sends to incorporate how conds automatically lock/unlock mutexes

--- lib/chan.c
+++ lib/chan.c
@@ -22,53 +22,63 @@
     }
 } 
 
+bool try_chan_send_locked(*Channel ch, *Value v) {
+    pthread_cond_t *recv_cond = ch->recvq->cond;
+    
+    if (recv_cond != NULL) {
+        // If there is a reciever waiting for a value,
+        // drop that reciever from the queue to claim it. 
+        ch->recvq = ch->recvq->next;
+        if (ch->recvq = NULL) {
+            ch->recvq = {
+                NULL,
+                NULL,
+            }
+        }
+        // Set the value of this channel to be the 
+        // value sent in, then unlock that reciever's
+        // thread so they can properly recieve it.
+        ch->v = v;
+        // Wake the waiting reciever up
+        pthread_cond_signal(recv_cond);
+        return true;
+    }
+    return false;
+}
+
+bool try_chan_send(*Channel ch, *Value v) {
+    pthread_mutex_lock(ch->lock);
+    bool b = try_chan_send_locked(ch, v);
+    pthread_mutex_unlock(ch->lock);
+    return b;
+}
+
 void chan_send(*Channel ch, *Value v) {
     Value *v2 = ch->v;
     if (v2->type != v->type) {
         // Panic
     }
-    while(true){
-        pthread_mutex_lock(ch->lock);
-        pthread_cond_t *recv_cond = ch->recvq->cond;
-        
-        if (recv_cond != NULL) {
-            // If there is a reciever waiting for a value,
-            // drop that reciever from the queue to claim it. 
-            ch->recvq = ch->recq->next;
-            if (ch->recvq = NULL) {
-                ch->recvq = {
-                    NULL,
-                    NULL,
-                }
-            }
-            // Set the value of this channel to be the 
-            // value sent in, then unlock that reciever's
-            // thread so they can properly recieve it.
-            ch->v = v;
-            // Wake the waiting sender up
-            pthread_cond_signal(recv_cond);
-            return;
-        }
-        // If there is not a waiting reciever
-        pthread_mutex_unlock(ch->lock)
-
-        // If no reciever is waiting for a value,
-        // this routine needs to wait until one
-        // shows up so it can send v to it and
-        // sync up. 
-
-        // This implementation uses a conditional
-        // wait until there is an available reciever.
-        
-        // Go to end of sendq
-        ThreadQueue *send_lock = ch->sendq;
-        while (send_lock->next != NULL) {
-            send_lock = send_lock->next;
-        }
-        send_lock->cond = &PTHREAD_COND_INITIALIZER;
-        
-        pthread_cond_wait(send_lock->cond,ch->lock);
-            
+    if (try_send_chan(ch, v)) {
+        return
     }
+    // If no reciever is waiting for a value,
+    // this routine needs to wait until one
+    // shows up so it can send v to it and
+    // sync up. 
+
+    // This implementation uses a conditional
+    // wait until there is an available reciever.
     
+    // Go to end of sendq
+    ThreadQueue *send_lock = ch->sendq;
+    while (send_lock->next != NULL) {
+        send_lock = send_lock->next;
+    }
+    send_lock->cond = &PTHREAD_COND_INITIALIZER;
+    
+    pthread_mutex_lock(ch->lock);
+    // cond_wait unlocks, and locks again after it returns
+    pthread_cond_wait(send_lock->cond,ch->lock);
+    try_chan_send_locked(ch, v);
+    pthread_mutex_unlock(ch->lock);
 }
