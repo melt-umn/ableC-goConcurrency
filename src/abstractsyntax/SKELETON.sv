94a2bcabfd0a16081a8e1c8395ad26a920a4220d
StephenPatrick <sythe2o0@gmail.com>
2017-02-17 11:59:57 -0600

    Terminals, a production

--- src/abstractsyntax/SKELETON.sv
+++ src/abstractsyntax/SKELETON.sv
@@ -2,127 +2,16 @@ grammar edu:umn:cs:melt:exts:ableC:goConcurrency:src:abstractsyntax;
 
 imports edu:umn:cs:melt:ableC:abstractsyntax;
 
+nonterminal Expr with pp, value;
 
-abstract production skeleton
-s::Stmt ::= loc::Location
+abstract production open
+op::Expr ::= loc::Location
 {
-  local printStmts :: [Stmt] 
-    = map (mkSkeletonLinePrint(loc,_), skeletonLines);
-
-  forwards to 
-    foldl1 (seqStmt, printStmts);
-
-  -- The skeleton, as a list of strings.  The need to escape
-  -- backslashes and double quotes obscures things a bit.
-  local skeletonLines :: [ String ] =
-   [ "                 .-\\\"```\\\"-.     ",
-     "                /         \\\\    ",
-     "                |  _   _  |    ",
-     "                | (_\\\\ /_) |    ",
-     "                (_   A   _)    ",
-     "                 | _____ |     ",
-     "                 \\\\`\\\"\\\"\\\"\\\"\\\"`/     ",
-     "                  '-.-.-'      ",
-     "                   _:=:_                   \\\\\\\\|  ",
-     "            .-\\\"\\\"\\\"\\\"`_'='_`\\\"\\\"\\\"\\\"-.           \\\\///  ",
-     "           (`,-- -`\\\\   /`- --,`)          (`/  ",
-     "           / //`-_--| |--_-`\\\\\\\\ \\\\         .//  ",
-     "          / /(_-_  _| |_  _-_)\\\\ \\\\       ///  ",
-     "         / / (_- __ \\\\ / __ -_) \\\\ \\\\     ///  ",
-     "        / /  (_ -_ - ^ - _- _)  \\\\ \\\\   ///  ",
-     "       / /   (_-  _ /=\\\\ _ - _)   \\\\ \\\\ '//  ",
-     "      / /     (_ -.':=:'. -_)     \\\\ \\\\//  ",
-     "     (`;`     (_-'  :=:  '-_)      (_,'  ",
-     "      \\\\\\\\.   jgs __  :=:  __     ",
-     "       \\\\\\\\\\\\    .'  `':=:'`  '.     ",
-     "        \\\\\\\\\\\\  |  .--. = .--.  |    ",
-     "         \\\\\\\\\\\\ |  (  / = \\\\  )  |    ",
-     "          \\\\\\\\` \\\\ _`' \\\\=/ '`_ /    ",
-     "          ;`)  ( ;_/`v`\\\\_; )    ",
-     "          |||\\\\ | |       | |    ",
-     "          |\\\\\\\\  | |       | |    ",
-     "               | |       | |    ",
-     "               | |       | |    ",
-     "               | |       | |    ",
-     "               | |       | |    ",
-     "               | |       | |    ",
-     "              (._)       (_.)    ",
-     "               ||,       ,||    ",
-     "               ||:       :||    ",
-     "               ||:       :||    ",
-     "               ||:       :||    ",
-     "               ||:       :||    ",
-     "               ||'       '||    ",
-     "              ///)       (\\\\\\\\\\\\    ",
-     "            .///`         `\\\\\\\\\\\\.    ",
-     "           `//`             `\\\\\\\\`    ",
-     "",
-     "     Courtesy of http://ascii-art.com."
-   ] ;
-}
-
-function mkSkeletonLinePrint
-Stmt ::= loc::Location skeletonLine::String
-{
-  local strForPrintf :: String =
-    "\"" ++ skeletonLine ++ "\\n\"" ;
-
-  return
-    exprStmt(
+  op.value = exprStmt(
       directCallExpr( 
-        name("printf", location=loc),
-        consExpr( stringLiteral(strForPrintf, location=loc),
-	          nilExpr() ),
+        name("chan_open", location=loc),
+        nilExpr(),
         location=loc
       )
-    ) ;
+    )
 }
-
-
-
-{-
-                 .-"```"-.
-                /         \
-                |  _   _  |
-                | (_\ /_) |
-                (_   A   _)
-                 | _____ |
-                 \`"""""`/
-                  '-.-.-'
-                   _:=:_                   \\|
-            .-""""`_'='_`""""-.           \///
-           (`,-- -`\   /`- --,`)          (`/
-           / //`-_--| |--_-`\\ \         .//
-          / /(_-_  _| |_  _-_)\ \       ///
-         / / (_- __ \ / __ -_) \ \     ///
-        / /  (_ -_ - ^ - _- _)  \ \   ///
-       / /   (_-  _ /=\ _ - _)   \ \ '//
-      / /     (_ -.':=:'. -_)     \ \//
-     (`;`     (_-'  :=:  '-_)      (_,'
-      \\.   jgs __  :=:  __
-       \\\    .'  `':=:'`  '.
-        \\\  |  .--. = .--.  |
-         \\\ |  (  / = \  )  |
-          \\` \ _`' \=/ '`_ /
-          ;`)  ( ;_/`v`\_; )
-          |||\ | |       | |
-          |\\  | |       | |
-               | |       | |
-               | |       | |
-               | |       | |
-               | |       | |
-               | |       | |
-              (._)       (_.)
-               ||,       ,||
-               ||:       :||
-               ||:       :||
-               ||:       :||
-               ||:       :||
-               ||'       '||
-              ///)       (\\\
-            .///`         `\\\.
-           `//`             `\\`
-
-     Courtesy of http://ascii-art.com.
--}
-
--- src/concretesyntax/SKELETON.sv
+++ src/concretesyntax/SKELETON.sv
@@ -5,10 +5,17 @@ imports silver:langutil only ast;
 
 imports edu:umn:cs:melt:exts:ableC:goConcurrency:src:abstractsyntax;
 
-marking terminal Skeleton_t 'skeleton!' ;
+terminal Open_t 'open`;
+terminal Close_t 'close';
+terminal Chan_t 'chan';
+terminal Arrow_t '<-';
+terminal Spawn_t 'spawn';
+terminal Select_t 'select';
+terminal Case_t 'case';
+terminal Default 'default';
 
-concrete production skeleton_c
-s::Stmt_c ::= sk::Skeleton_t
+concrete production open_c
+s::Stmt_c ::= sk::Open_t sk::Chan_t
 {
-  s.ast = skeleton( sk.location );
+  s.ast = open( sk.location );
 }
