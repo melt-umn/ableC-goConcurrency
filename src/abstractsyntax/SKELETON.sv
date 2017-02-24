0bf15c367583469d8b6f6819ea9827c5f53481df
StephenPatrick <sythe2o0@gmail.com>
2017-02-24 11:31:33 -0600

    Testing out silver

--- src/abstractsyntax/SKELETON.sv
+++ src/abstractsyntax/SKELETON.sv
@@ -1,17 +1,18 @@
 grammar edu:umn:cs:melt:exts:ableC:goConcurrency:src:abstractsyntax;
 
-imports edu:umn:cs:melt:ableC:abstractsyntax;
+imports edu:umn:cs:melt:ableC:abstractsyntax as abs;
+imports edu:umn:cs:melt:ableC:abstractsyntax:construction as abs;
+imports edu:umn:cs:melt:ableC:abstractsyntax:env as abs;
 
-nonterminal Expr with pp, value;
+imports silver:langutil;
 
 abstract production open
-op::Expr ::= loc::Location
+top::abs:Expr ::= text::abs:Expr
 {
-  op.value = exprStmt(
+  forwards to 
       directCallExpr( 
-        name("chan_open", location=loc),
-        nilExpr(),
-        location=loc
-      )
-    )
+        name("chan_open", location=top.location),
+        nil,
+        location=top.location
+      );
 }
