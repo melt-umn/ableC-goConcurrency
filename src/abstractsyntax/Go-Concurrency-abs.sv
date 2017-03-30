11aee6813de734f9ddf8b5944914948698aa9f6a
StephenPatrick <sythe2o0@gmail.com>
2017-03-29 22:16:20 -0500

    Grammar changes to disambigulate recv and send
    I think Go does this disambigulation through meaningful whitespace ( ch <- ch is send, x = <-ch is recv)
    
    Changed sieve to use the things we can compile through silver so far, still missing the function spawning piece.

--- src/abstractsyntax/Go-Concurrency-abs.sv
+++ src/abstractsyntax/Go-Concurrency-abs.sv
@@ -7,25 +7,26 @@ imports edu:umn:cs:melt:exts:ableC:templating:abstractsyntax as tmp;
 imports silver:langutil;
 
 abstract production open
-top::Expr ::= 
+top::Expr ::= t::TypeName
 {
   forwards to 
-      directCallExpr( 
-        name("chan_open", location=top.location),
-        nilExpr(),
-        location=top.location
-      );
+      callExpr(
+          tmp:templateDeclRefExpr(name("chan_open",location=top.location), 
+              consTypeName(t, nilTypeName()),location=top.location),
+          nilExpr(), location=top.location);
 }
 
 abstract production close
-top::Expr ::= e::Expr
+top::Expr ::= ch::Expr
 {
-  forwards to
-      directCallExpr( 
-        name("chan_close", location=top.location),
-        consExpr(e, nilExpr()),
-        location=top.location
-      );
+  local channelType::Type = channelSubType(ch.typerep, ch.env);
+
+  forwards to 
+      callExpr(
+          tmp:templateDeclRefExpr(name("chan_close",location=top.location), 
+              consTypeName(typeName(directTypeExpr(channelType), baseTypeExpr()),
+                   nilTypeName()),location=top.location),
+          consExpr(ch, nilExpr()), location=top.location);
 }
 
 abstract production send
@@ -42,7 +43,6 @@ top::Expr ::= ch::Expr v::Expr
 abstract production recieve
 top::Expr ::= ch::Expr
 {
-
   local channelType::Type = channelSubType(ch.typerep, ch.env);
 
   forwards to 
@@ -52,3 +52,13 @@ top::Expr ::= ch::Expr
                    nilTypeName()),location=top.location),
           consExpr(ch, nilExpr()), location=top.location);
 }
+
+--abstract production spawnFunction
+--top::Expr ::= args::Exprs f::Expr 
+--{
+  -- we need to create a function which will
+  -- pull out everything in args out of a void * *
+  -- and call f. 
+  -- we then need to call that function with spawn_function
+  -- and the args packed into a void * *. 
+--}
--- src/concretesyntax/Go-Concurrency-cnc.sv
+++ src/concretesyntax/Go-Concurrency-cnc.sv
@@ -5,7 +5,7 @@ imports silver:langutil only ast;
 
 imports edu:umn:cs:melt:exts:ableC:goConcurrency:src:abstractsyntax;
 
-marking terminal Arrow_t '<-' precedence = 7, association = left, lexer classes{Csymbol};
+marking terminal Arrow_t '<-' lexer classes{Ckeyword};
 marking terminal Open_t 'open' lexer classes {Ckeyword};
 marking terminal Close_t 'close' lexer classes {Ckeyword};
 terminal Chan_t 'chan';
@@ -15,9 +15,9 @@ terminal Case_t 'case';
 terminal Default_t 'default';
 
 concrete production open_c
-s::PrimaryExpr_c ::= Open_t Chan_t
+s::PrimaryExpr_c ::= Open_t Chan_t '<' t::TypeName_c '>'
 {
-  s.ast = open(location=s.location);
+  s.ast = open(t.ast, location=s.location);
 }
 
 concrete production close_c
@@ -27,7 +27,7 @@ s::PrimaryExpr_c ::= Close_t '(' e::AssignExpr_c ')'
 }
 
 concrete production recieveFrom_c
-s::Expr_c ::= '<-' ch::Expr_c
+s::PrimaryExpr_c ::= '(' '<-' ch::Expr_c ')'
 {
   s.ast = recieve(ch.ast, location=s.location);
 }
@@ -45,7 +45,7 @@ s::Expr_c ::= ch::AssignExpr_c '<-' v::AssignExpr_c
 --}
 
 --concrete productions spawn_function_c
---s::Expr_c ::= Spawn_t '( lambda::Expr_c )' ':' '( args::ArgumentExprList_c )'
+--s::Expr_c ::= Spawn_t lambda::Expr_c '(' args::ArgumentExprList_c ')'
 --{ 
 --  top.ast = spawnFunction(foldExpr(args.ast), lambda.ast, location=top.location); 
 --}
