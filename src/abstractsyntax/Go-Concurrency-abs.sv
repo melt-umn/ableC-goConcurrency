e04c618f03321eaf019c2df0a21f2ba2ac4f1de0
StephenPatrick <sythe2o0@gmail.com>
2017-04-07 12:22:54 -0500

    Spawn function abstract syntax (not run)

--- src/abstractsyntax/Go-Concurrency-abs.sv
+++ src/abstractsyntax/Go-Concurrency-abs.sv
@@ -62,3 +62,96 @@ top::Expr ::= ch::Expr
   -- we then need to call that function with spawn_function
   -- and the args packed into a void * *. 
 --}
+
+global builtin::Location = builtinLoc("go_conc");
+
+abstract production spawnFunction
+top::Stmt ::= args::Exprs res::Expr
+{
+
+  local id::String = toString(genInt()); 
+  local argStructName::String = s"_spawn_arg_${id}_s";
+  local funName::String = s"_spawn_fn_${id}";
+  
+  local argStructDcl::Decl =
+    typeExprDecl([],
+      structTypeExpr([],
+        structDecl([],
+          justName(name(argStructName, location=builtin)),
+          argsToStructItems(args, 0),
+          location=builtin)));
+  
+  local funDcl::Decl =
+    subDecl(
+      [stmtSubstitution("__call__", callFromStruct(funName, argStructName)],
+      decls(
+        parseDecls(s"""
+            static void * ${funName}(void *_arg_ptr) {
+                struct ${argStructName} _env = 
+                    *(struct ${argStructName}*)_arg_ptr;
+                __call__;
+            }""")));
+
+  local globalDecls::Decls = foldDecl([argStructDcl, funDcl]);
+ 
+  local structVarName::String = s”_spawn_struct_var_${id}_s”;
+  local structVarExpr::Expr = parseExpr(s”””${structVarName}”””);
+
+  Local actual_forwards :: Stmt = seqStmt(
+      makeArgStruct(args,argStructName,structVarName),
+        exprStmt(
+            directCallExpr(“spawn_function”,
+                consExpr(
+                    parseExpr(s”””${funName}”””), consExpr(
+                        explicitCastExpr(typeName(
+                            directTypeExpr(builtinType([],voidType()), 
+                                pointerTypeExpr([], baseTypeExpr()))), 
+                            structVarExpr), 
+                        nilExpr())))));
+
+  Forwards to injectGlobalDeclsStmt (  globalDecls, actual_forwards );
+}
+
+-- return a filled in struct of type structName with the given args
+function makeArgStruct
+Stmt::= args:Exprs structName::Name varName::Name
+{	
+	seqStm(parseStmt(s”””malloc(sizeof(${structName}))”””),
+                         fillArgs(args,varName, 0));
+}
+
+function fillArgs
+Stmt::= args::Exprs varName::Name count::Integer
+{
+   headStmt::Stmt = subStmt([exprSubstitution(“__expr__”, head(args))],
+                    parseStmt(s”””${varName}.${count} = __expr__”””));
+
+   return if args.count == 1 then headStmt  
+               else seqStmt(headStmt, fillArgs(tail(args),varName, count+1));
+}
+
+-- put all of the args from the struct and call the function
+function callFromStruct
+Stmt ::= funName::Name args::Exprs
+{ 
+   directCallExpr(funName, paramsFromStruct(args, 0));  
+}
+
+function paramsFromStruct
+Exprs ::= args::Exprs count::Integer
+{
+    return consExpr(parseExpr(s”””_env.f${count}”””), 
+              If args.count == 1 then nilExpr()
+                 else fieldsFromStruct(tail(args), count+1));
+}
+
+-- returns a list of struct items from the list of arguments passed in
+function argsToStructItems
+StructItemList ::= args::Exprs count::Integer
+{
+  return foldr(consStructItem, structItem([], head(args).typerep, 
+        structField(name(s”f${count}”,location=head(args).location),
+            nilTypeModifierExpr(), []))
+            if args.count == 1 then []
+            else argsToStructItems(tail(args), count+1));
+}
