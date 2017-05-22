0be0bca731e757139e7a37bd546af136ee5fb947
StephenPatrick <sythe2o0@gmail.com>
2017-05-22 12:10:40 -0500

    MDA tests done before select recieve, issue with follow set vs AddMulOp_c

--- grammars/edu.umn.cs.melt.exts.ableC.goConcurrency/concretesyntax/selectExpr/SelectExpr_cnc.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.goConcurrency/concretesyntax/selectExpr/SelectExpr_cnc.sv
@@ -4,7 +4,8 @@ imports edu:umn:cs:melt:ableC:abstractsyntax;
 imports edu:umn:cs:melt:ableC:concretesyntax;
 imports silver:langutil only ast; 
 imports edu:umn:cs:melt:exts:ableC:goConcurrency:abstractsyntax;
-imports edu:umn:cs:melt:exts:ableC:goConcurrency:concretesyntax:arrow   ;
+
+exports edu:umn:cs:melt:exts:ableC:goConcurrency:concretesyntax:arrow;
 
 terminal SelectArrow_t '<-' lexer classes{Cassignment}, association = left;
 terminal EqArrow '=<-' lexer classes{Cassignment};
@@ -16,15 +17,20 @@ disambiguate Arrow_t, SelectArrow_t {
 closed nonterminal SelectExpr_c with location, ast<Expr>;
 
 concrete productions s::SelectExpr_c
-| SelectArrow_t ch::AssignExpr_c
+| SelectArrow_t ch::UnaryExpr_c
 {
   s.ast = tryReceive(ch.ast, location=s.location);
 }
-| ch::PrimaryExpr_c SelectArrow_t v::PrimaryExpr_c
-{   
-  s.ast = trySend(ch.ast, v.ast, location=s.location);
-}
-| v::PrimaryExpr_c '=' SelectArrow_t ch::PrimaryExpr_c 
+--| ch::UnaryExpr_c SelectArrow_t v::UnaryExpr_c
+--{   
+--  s.ast = trySend(ch.ast, v.ast, location=s.location);
+--}
+| v::UnaryExpr_c '=' SelectArrow_t ch::UnaryExpr_c 
 {
   s.ast = tryAssign(ch.ast, v.ast, location=s.location);
 }
+
+-- We need to create a new binary operation type? 
+-- The issue becomes avoiding a conflict with x arrow_t y 
+-- and x selectarrow_t y, if both of them are just AddMulOp_c
+ 
