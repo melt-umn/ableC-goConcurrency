a0d926d3d8708301d11e25f7ed90b6b96206c05a
StephenPatrick <sythe2o0@gmail.com>
2017-05-22 15:39:21 -0500

    Removing split grammars pt1

--- grammars/edu.umn.cs.melt.exts.ableC.goConcurrency/concretesyntax/selectCases/SelectCases_cnc.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.goConcurrency/concretesyntax/selectCases/SelectCases_cnc.sv
@@ -1,10 +1,34 @@
 grammar edu:umn:cs:melt:exts:ableC:goConcurrency:concretesyntax:selectCases;
 
+imports edu:umn:cs:melt:ableC:abstractsyntax;
 imports edu:umn:cs:melt:ableC:concretesyntax;
 imports silver:langutil only ast; 
 imports edu:umn:cs:melt:exts:ableC:goConcurrency:abstractsyntax;
 
-exports edu:umn:cs:melt:exts:ableC:goConcurrency:concretesyntax:selectExpr;
+exports edu:umn:cs:melt:exts:ableC:goConcurrency:concretesyntax:arrow;
+
+terminal SelectArrow_t '<-' lexer classes{Cassignment}, association = left;
+terminal EqArrow '=<-' lexer classes{Cassignment};
+
+disambiguate Arrow_t, SelectArrow_t {
+  pluck Arrow_t;
+}
+
+closed nonterminal SelectExpr_c with location, ast<Expr>;
+
+concrete productions s::SelectExpr_c
+| SelectArrow_t ch::AssignExpr_c
+{
+  s.ast = tryReceive(ch.ast, location=s.location);
+}
+| ch::PrimaryExpr_c  op::AddMulNoneOp_c v::UnaryExpr_c
+{   
+  s.ast = trySend(ch.ast, v.ast, location=s.location);
+}
+| v::UnaryExpr_c '=' SelectArrow_t ch::UnaryExpr_c 
+{
+  s.ast = tryAssign(ch.ast, v.ast, location=s.location);
+}
 
 nonterminal SelectCases_c with location, ast<SelectCases>;
 
